161301f0708f5d8e6396c645bce4e8514c76ed1f
SaadBadr <saadbadr23@gmail.com>
2021-12-12 11:05:18 +0200

    update course controller and routes
    adding getCourse and createCourse in the course controller and their routes in course routes

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,3 +3,36 @@ const AppError = require("../utils/appError");
 const DbQueryManager = require("../utils/dbQueryManager");
 const Course = require("../models/CourseModel");
 
+module.exports.courseCreate = catchAsync(async (req, res, next) => {
+  const { syllabus, title } = req.body;
+  const instructor = req.user._id;
+
+  let newCourse = new Course({ syllabus, title, instructor });
+  newCourse = await newCourse.save();
+
+  res.status(200).json({
+    status: "success",
+    course: newCourse,
+  });
+});
+
+module.exports.courseGet = catchAsync(async (req, res, next) => {
+  const courseId = req.params.id;
+  const userId = req.user._id;
+  const userType = req.user.type;
+  const course = await Course.findById(courseId);
+  console.log(course, courseId, userId, userType);
+
+  if (
+    userType != "admin" &&
+    course.instructor != userId &&
+    !user.enrolledCourses.includes(courseId)
+  ) {
+    throw new AppError("You are not allowed to access this course", 401);
+  }
+
+  res.status(200).json({
+    status: "success",
+    course,
+  });
+});
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,4 +4,16 @@ const courseController = require("./../controllers/courseController");
 
 const router = express.Router();
 
+router
+  .route("/")
+  .post(
+    authenticationController.protect(),
+    authenticationController.restrictTo("admin", "instructor"),
+    courseController.courseCreate
+  );
+
+router
+  .route("/:id")
+  .get(authenticationController.protect(), courseController.courseGet);
+
 module.exports = router;
