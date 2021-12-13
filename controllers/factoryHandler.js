7499521f476dcea3f2d69649d045cef52222e126
SaadBadr <saadbadr23@gmail.com>
2021-12-13 20:51:48 +0200

    Create factoryHandler.js
    factory handles similar controller functions

---
+++ controllers/factoryHandler.js
@@ -0,0 +1,14 @@
+const catchAsync = require("../utils/catchAsync")
+const AppError = require("../utils/appError")
+const DbQueryManager = require("../utils/dbQueryManager")
+
+module.exports.createOneFactory = (model) =>
+  catchAsync(async (req, res, next) => {
+    let data = new model(req.body)
+    data = await data.save()
+
+    res.status(200).json({
+      status: "success",
+      data,
+    })
+  })
